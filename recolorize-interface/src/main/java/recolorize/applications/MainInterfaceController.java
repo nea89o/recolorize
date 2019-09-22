a87ddb0bcb6e60d8af6213264e5481b79a561320
Nik1as <oberender.niklas@gmail.com>
2019-09-22 15:08:07 +0200

    formating

--- recolorize-interface/src/main/java/recolorize/applications/MainInterfaceController.java
+++ recolorize-interface/src/main/java/recolorize/applications/MainInterfaceController.java
@@ -7,7 +7,6 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
-import org.nd4j.linalg.indexing.NDArrayIndex;
 import recolorize.image.PixelImage;
 import recolorize.neuralnetwork.ColorizerNeuralNetwork;
 
